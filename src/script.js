c37a79650e5a68d5e9076b4cc931d857926e1374
Kuba Karpierz <mr.karpierz@gmail.com>
2023-12-26 12:19:02 -0500

    new vertex add

--- src/script.js
+++ src/script.js
@@ -42,6 +42,12 @@ class Plane {
         this.position = position;
         this.normal = normal;
     }
+
+    cuts(start, end) {
+        let startDist = start.sub(this.position).dot(this.normal);
+        let endDist = end.sub(this.position).dot(this.normal);
+        return endDist * startDist < 0.0;
+    }
 }
 
 class HalfEdge {
@@ -55,16 +61,30 @@ class HalfEdge {
 }
 
 class DcelMesh {
+    getVertex(index) {
+        return this.vertices[index].clone();
+    }
+
     getEdge(start, end) {
-        let index = start + this.vertices.length * end;
+        let index = start.toString() + "+" + end.toString();
         if (!this.edges.has(index)) {
             return null;
         }
         return this.edges.get(index)
     }
 
+    addVertex(vertex) {
+    let existingIndex =   this.vertices.find(vertex)
+    if (existingIndex >= 0 ) {
+        return existingIndex;
+    } else {
+        this.vertices.push(vertex);
+        return this.vertices.length - 1;
+    }
+    }
+
     addEdge(edge) {
-        let index = edge.start + this.vertices.length * edge.end;
+        let index = edge.start.toString() + "+" + edge.end.toString();
         this.edges.set(index, edge);
         let twin = this.getEdge(edge.end, edge.start);
         if (twin) {
@@ -74,19 +94,30 @@ class DcelMesh {
     }
 
     constructor(vertices, faces) {
-        this.vertices = vertices;
+        var normalizedVertices = [];
+        var indexRemapping = new Map();
+        for (let i = 0; i < vertices.length; i++) {
+            let vertex = new THREE.Vector3().fromArray(vertices, 3*i)
+            let existingIndex = normalizedVertices.findIndex((v) => {
+                return v.equals(vertex);
+            })
+            if (existingIndex >= 0) {
+                indexRemapping.set(i, existingIndex);
+            } else {
+                indexRemapping.set(i, normalizedVertices.length);
+                normalizedVertices.push(vertex);
+            }
+        }
+        var remappedFaces = faces.map(face => (face.map(i => indexRemapping.get(i))));
+        this.vertices = normalizedVertices;
         this.edges = new Map();
-        for (let j = 0; j < faces.length; j++) {
-            let face = faces[j]
+        for (let face of remappedFaces) {
             for (let i = 0; i < face.length; i++) {
                 let start = face[i];
                 let end = face[(i + 1) % face.length];
                 this.addEdge(new HalfEdge(start, end));
             }
             for (let i = 0; i < face.length; i++) {
-                let curr = face[i];
-                let next1 = face[(i + 1) % face.length];
-                let next2 = face[(i +2) % face.length];
                 let prev = this.getEdge(face[i], face[(i + 1) % face.length]);
                 let next = this.getEdge(face[(i + 1) % face.length],face[(i + 2) % face.length]);
                 prev.next = next;
@@ -96,10 +127,72 @@ class DcelMesh {
     }
 
     cut(plane) {
+        var iterations = 0;
+        // Find a cut edge:
+        for (let edge of this.edges.values()) {
+            iterations++;
+            if (!plane.cuts(this.getVertex(edge.start),this.getVertex(edge.end))) {
+                continue;
+            }
 
+            var cutEdges = [edge];
+            var next = edge.next;
+            // edge is cut. Find edges that are going to be cut, in order.
+            while (true) {
+                if (iterations >= 100000) {
+                    console.log("ITERATIONS EXCEEDED");
+                    break;
+                }
+                iterations++;
+                while (!plane.cuts(this.getVertex(next.start),this.getVertex(next.end))) {
+                    
+                    if (iterations >= 100000) {
+                        console.log(next);
+                        console.log(cutEdges);
+                        console.log("ITERATIONS EXCEEDED");
+                        break;
+                    }
+                    iterations++;
+                    next = next.next;
+                }
+                cutEdges.push(next);
+                next = next.twin;
+                if (next === edge) {
+                    break;
+                }
+                cutEdges.push(next);
+                next = next.next;
+            }
+            console.log(next);
+            console.log(cutEdges);
+
+            for (let i = 0; i < cutEdges.length; i += 2) {
+                let first = cutEdges[i];
+                let second = cutEdges[i+1];
+
+
+            }
+
+            // each pair of edges belongs to the same face. 
+            // go through each pair, split them, add two new half edges, and then store them
+            break;
+        }
     }
 
     toVertices() {
+        let [vertices, indices] = this.toVerticesIndices();
+        console.log(vertices);
+        console.log(indices)
+        let vertexArray = new Float32Array(indices.length * 3);
+        indices.forEach((vertexIndex, index) => {
+            vertexArray[3*index] = vertices[3*vertexIndex]
+            vertexArray[3*index+1] = vertices[3*vertexIndex+1]
+            vertexArray[3*index+2] = vertices[3*vertexIndex+2]
+        })
+        return vertexArray;
+    }
+
+    toVerticesIndices() {
         const traversedEdges = new Map();
         var indices = [];
         for (let edge of this.edges.values()) {
@@ -116,8 +209,9 @@ class DcelMesh {
                 next = next.next;
             }
         }
-
-        return [new Float32Array(this.vertices), indices];
+        let vertexArray = new Float32Array(this.vertices.length * 3);
+        this.vertices.forEach((v, i) => v.toArray(vertexArray, 3*i));
+        return [vertexArray, indices];
     }
 }
 
@@ -190,7 +284,8 @@ controls.enableRotate = false;
  */
 const mouse = {
     start: null,
-    end: null
+    end: null,
+    justReleased: false
 }
 
 const mousePos = (event) => {
@@ -211,6 +306,7 @@ window.addEventListener('pointerdown', (event) => {
 window.addEventListener('pointerup', (event) => {
     mouse.start = null;
     mouse.end = null;
+    mouse.justReleased=  true;
 });
 
 /**
@@ -260,35 +356,6 @@ loadingManager.onProgress = (_, itemsLoaded, itemsTotal) =>
         timeline.to(overlayMaterial.uniforms.uMinY, {duration: 0.6, value: 0.5, ease: 'power1.in'})
     }
  };
-/**
- * Plane cut
- */
-
-const raycaster = new THREE.Raycaster(camera.position);
-raycaster.layers.set(1);
-const cut = {
-    startHit: null,
-    startNormal: null,
-    endHit: null
-}
-const updateCut = () => {
-    if (mouse.start) {
-        raycaster.setFromCamera( mouse.start, camera );
-        const intersects = raycaster.intersectObjects( scene.children );
-        if (intersects.length > 0) {
-            cut.startHit = intersects[0].point;
-            cut.startNormal = intersects[0].normal;
-        }
-    }
-    
-    if (mouse.end) {
-        raycaster.setFromCamera( mouse.end, camera );
-        const intersects = raycaster.intersectObjects( scene.children );
-        if (intersects.length > 0) {
-            cut.endHit = intersects[0].point;
-        }
-    }
-}
 
 /**
  *  Box
@@ -306,14 +373,11 @@ var vertices = Array.from(boxGeo.attributes.position.array);
 var indices = splitToNChunks(Array.from(boxGeo.index.array), 3);
 var dcelMesh = new DcelMesh(vertices, indices);
 
-let [vertices2, indices2] = dcelMesh.toVertices();
-
-console.log(vertices2);
-console.log(indices2);
+let vertices2 = dcelMesh.toVertices();
 
 const boxG = new THREE.BufferGeometry();
-boxG.setIndex(indices2)
 boxG.setAttribute( 'position', new THREE.BufferAttribute(vertices2, 3))
+boxG.computeVertexNormals()
 boxG.computeBoundingBox();
 const boxM = new THREE.ShaderMaterial({
     vertexShader: crackVertexShader, 
@@ -338,6 +402,48 @@ scene.add(boxMesh)
 const rotateBox = (time) => {
     boxMesh.setRotationFromEuler(new THREE.Euler(0, time, 0)) 
 }
+
+/**
+ * Plane cut
+ */
+
+const raycaster = new THREE.Raycaster(camera.position);
+raycaster.layers.set(1);
+const cut = {
+    startHit: null,
+    startNormal: null,
+    endHit: null
+}
+
+const updateCut = () => {
+    if (mouse.start) {
+        raycaster.setFromCamera( mouse.start, camera );
+        const intersects = raycaster.intersectObjects( scene.children );
+        if (intersects.length > 0) {
+            cut.startHit = intersects[0].point;
+            cut.startNormal = intersects[0].normal;
+        }
+    }
+    
+    if (mouse.end) {
+        raycaster.setFromCamera( mouse.end, camera );
+        const intersects = raycaster.intersectObjects( scene.children );
+        if (intersects.length > 0) {
+            cut.endHit = intersects[0].point;
+        }
+    }
+
+    if (mouse.justReleased) {
+        var diff = new THREE.Vector3();
+        diff.subVectors(cut.startHit, cut.endHit);
+        var planeNormal = new THREE.Vector3();
+        planeNormal.crossVectors(cut.startNormal, diff).normalize();
+        let plane = new Plane(cut.startHit, planeNormal);
+        dcelMesh.cut(plane)
+        mouse.justReleased= false;
+    }
+}
+
 
 /**
  * Animation
